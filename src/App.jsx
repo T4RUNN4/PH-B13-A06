b1b3d491dc7a9e8212cddcae38134678c9b4a5f0
Tarunna <tarunnasen@gmail.com>
2026-03-30 03:24:05 +0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./sections/Header";
 import Main from "./sections/Main";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
     </>
   );
 }
