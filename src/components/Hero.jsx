b15fd44d975972ad595667eef937cdf0c59b4eac
Tarunna <tarunnasen@gmail.com>
2026-03-30 02:24:08 +0600

    Count Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@ import Play from "../assets/play.png";
 
 export default function Hero() {
   return (
-    <div className="hero bg-base-200 md:px-32 min-h-screen">
+    <div className="hero md:px-32 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={HeroImage} className="max-w-sm rounded-lg shadow-2xl" />
-        <div className="max-w-md text-center lg:text-left">
+        <div className="max-w-md text-center lg:text-left mt-4 lg:mt-0">
           <h1 className="text-5xl font-bold text-[#101727] leading-14">
             Supercharge Your Digital Workflow
           </h1>
@@ -17,7 +17,10 @@ export default function Hero() {
           <a className="btn btn-primary rounded-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] mr-4">
             Explore Products
           </a>
-          <a className="btn btn-ghost bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-transparent bg-clip-text border border-solid border-[#9514FA] rounded-full"> <img src={Play} /> Watch Demo</a>
+          <a className="btn btn-ghost bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-transparent bg-clip-text border border-solid border-[#9514FA] rounded-full">
+            {" "}
+            <img src={Play} /> Watch Demo
+          </a>
         </div>
       </div>
     </div>
---
+++ src/sections/Main.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+import Counts from "../components/Counts";
+
+export default function Main() {
+    return (
+        <main>
+            <Hero></Hero>
+            <Counts></Counts>
+        </main>
+    )
+}
