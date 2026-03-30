3ce554bb50094edb989ee9ba53910666730b79d5
Tarunna <tarunnasen@gmail.com>
2026-03-30 15:42:44 +0600

    Solved Sequence

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,15 +1,15 @@
 import Hero from "../components/Hero";
+import Counts from "../components/Counts";
 import ToolsContainer from "../components/ToolsContainer";
 import Process from "../components/Process";
-import Counts from "../components/Counts";
 
 export default function Main() {
   return (
     <main>
       <Hero></Hero>
+      <Counts></Counts>
       <ToolsContainer></ToolsContainer>
       <Process></Process>
-      <Counts></Counts>
     </main>
   );
 }
